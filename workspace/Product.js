e4faef5800a44319fbba3529214a6eee9cd44e6a
Oguz <oguzciftci@gmail.com>
2023-04-08 23:11:53 +0300

    yayında yaptıklarımız

--- workspace/Product.js
+++ workspace/Product.js
@@ -1,5 +1,6 @@
 module.exports = class Product {
-    constructor(name) {
+    constructor(id , name) {
+        this.id = id;
         this.name = name;
     }
 
--- workspace/app.js
+++ workspace/app.js
@@ -1,30 +1,53 @@
 const express = require('express');
 let Product = require("./Product.js");
+const bp = require('body-parser')
 //let Mongo = require("./Mongo.js");
 const {MongoClient} = require("mongodb");
 
 const app = express();
+
+app.use(bp.json())
+app.use(bp.urlencoded({ extended: true }))
+
 const uri = "mongodb://admin:password@localhost:27017/admin";
 
-app.get('/',  (req, res) => {
-    let product1 = new Product("Product 1");
-    let product2 = new Product("Product 2");
-    let product3 = new Product("Product 3");
-    var products = [];
+var products = [];
+
+app.get('/addall',  (req, res) => {
+    let product1 = new Product( "1", "Product 1");
+    let product2 = new Product("2" , "Product 2");
+    let product3 = new Product("3", "Product 3");
+
     products.push(product1, product2, product3);
+    //console.log("Response sended");
+    res.send("ok");
+});
 
+app.get('/all',  (req, res) => {
     let str = JSON.stringify(products);
-
-    connect();
-    console.log("Response sended");
     res.send(str);
 });
+
+app.post('/product',  (req, res) => {
+    let id = req.body.id;
+    let name = req.body.name;
+
+    let product1 = new Product(id, name);
+    products.push(product1);
+
+    let str = JSON.stringify(products);
+    res.send(str);
+});
+
+
 
 // Listen to the App Engine-specified port, or 8080 otherwise
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}...`);
 });
+
+
 
 
 async function connect(){
